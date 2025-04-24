fe39baf9f190666c2807bef5b01cb666317e2516
vaisakhnoff <vaisakhnofficial@gmail.com>
2025-04-24 19:18:54 +0530

    setup controllers , models , routes  and app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const path = require('path');
 const  dotenv = require('dotenv');
 const db = require('./config/db')
-
 dotenv.config();
 const app = express();
 
+const user = require('./routes/userRoute')
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
@@ -17,7 +17,7 @@ app.use(express.json());
 db();
 
 
-// app.use('/',user);
+app.use('/',user);
 
 app.listen(process.env.PORT,()=>{
     console.log("server running 3500");
---
+++ controllers/user/userController.js
@@ -0,0 +1,7 @@
+const getHome = async (req,res) => {
+    res.render('home');
+}
+
+const getProductDetail = async(req,res)=>{
+    res.render('productPage')
+}
